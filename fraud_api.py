eb1dda8b774fb5603ab3ca6b5b0880f60eecd428
Shyam <subediz420@gmail.com>
2025-03-07 17:54:26 -0500

    Update fraud_api.py

--- fraud_api.py
+++ fraud_api.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 # Load trained fraud detection model
 try:
-    model = joblib.load("final_fraud_detection_model_fixed.pkl")
+    model = joblib.load("final_fraud_detection_model_fixed.pkl(1)")
     print("✅ Model loaded successfully!")
 except Exception as e:
     print(f"❌ Error loading model: {str(e)}")
